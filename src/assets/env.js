669023fcd25010face273bddbde7380f451fe231
Viibur <viiburandre@gmail.com>
2023-05-07 23:14:11 +0300

    fix

--- src/assets/env.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-  window["env"] = window["env"] || {};
-
-  // Environment variables
-  window["env"]["apiUrl"] = "https://localhost:8080/";
-  window["env"]["debug"] = true;
-})(this);
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-// @ts-ignore
-  API_URL: window["env"]["apiUrl"]
-};
