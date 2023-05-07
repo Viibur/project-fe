af09d8ae168c6487cbdd411833a9d24372ddbf60
Viibur <viiburandre@gmail.com>
2023-05-07 23:17:07 +0300

    Auto-generated commit [ci skip]

--- src/app/juhend-modal/juhend-modal.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-juhend-modal',
-  templateUrl: './juhend-modal.component.html',
-  styleUrls: ['./juhend-modal.component.css']
-})
-export class JuhendModalComponent {
-}
--- src/app/model/interface.model.ts
+++
@@ -1,7 +0,0 @@
-export interface VeanaideDTO {
-  id: number,
-  viga: string,
-  korrektne: string,
-  lause: string,
-  sagedus: number,
-}
